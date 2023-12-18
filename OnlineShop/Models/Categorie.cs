ac146c6926f34c5ac334ab5465a51f275b63f939
Rs0V <--->
2023-12-18 23:26:54 +0200

    Adaugat Model Categorie

---
+++ OnlineShop/Models/Categorie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShop.Models
+{
+    public class Categorie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Denumirea categoriei este obligatorie")]
+        public string? Denumire { get; set; }
+    }
+}
