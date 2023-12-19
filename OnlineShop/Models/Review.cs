3093377d1329af62301b77c0ad80b9fde9d7b68d
Economu <economuvictor@gmail.com>
2023-12-19 14:31:10 +0200

    Review Tabel - Model

---
+++ OnlineShop/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace OnlineShop.Models
+{
+    public class Review
+    {
+        // Composite Primary Key
+        [Key, Column(Order = 0)]
+        public int UtilizatorId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int ProdusId { get; set; }
+        //
+
+        public string? Continut { get; set; }
+    }
+}
